08b60fc5bebf0ec9756e40b73bc248e0761500f3
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-01-21 22:43:35 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,power,result=1;
+printf("Enter the number:");
+scanf("%d%d",&num,&power);
+while(power!=0)
+{
+result=result*num;
+--power;
+}
+printf("%d",result);
+}
