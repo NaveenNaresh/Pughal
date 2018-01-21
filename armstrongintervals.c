70a952823236041ead3d59c61f71d70e21c94543
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-01-21 23:13:23 +0530

    Create armstrongintervals.c

---
+++ armstrongintervals.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,i,t=a-150,b=160,rem,result=0;
+printf("The first intervals %d",a);
+printf("The second intervals %d,b);
+{
+for(i=150;i<=160;i++)
+{
+t=1;
+while(n!=0)
+{
+rem=n%10;
+result=result+rem*rem*rem;
+n=n%10;
+}
+if(t==result)
+{
+printf("armstrong number between two intervals are %d",result);
+}
+}
+}
