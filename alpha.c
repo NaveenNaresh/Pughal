9aa0a4d5ece79676e3b156b04a9bd716b26069f7
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-01-21 16:09:20 +0530

    Update alpha.c

--- alpha.c
+++ alpha.c
@@ -5,7 +5,7 @@ char ch;
 printf("enter the letter:",ch);
 scanf("%d",&ch);
 {
-if((ch>=65 && ch <=90) || (ch >=92 && ch <= 122 ))
+if((ch>=65&&ch<=90)||(ch >=92&&ch<=122))
 printf("%c is a alphet,"ch);
 else 
 printf("%c is not a alphet,"ch);
