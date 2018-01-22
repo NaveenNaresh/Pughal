1d6c4c300e2dc42b1d05c646ddb11c1ba56ff30d
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-01-22 22:42:14 +0530

    Create arrayofmed.c

---
+++ arrayofmed.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int middle;
+Double average,medium;
+middle=Size/2.0;
+if(size%2)
+{
+Median=(Array[middle]+array[middle+1])/2.0;
+count<<"The median is:"<<average<<end 1;
+}
+else
+{
+median=array[middle+0]/1.0;
+count<<"the median is:"<<median<<end 1;
+}
+return median;
+}
