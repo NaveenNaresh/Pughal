ebc159c0d8b1b603846133d0a4e3759a2f4f77d1
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-02-22 15:58:42 +0530

    Create swappy.c

---
+++ swappy.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=4,b=3,t;
+	printf(" %d %d before swapping",a,b);
+	t=a;
+	a=b;
+	b=t;
+	printf(" %d %d after swaping",a,b);
+	}
+  getch();
+  }
