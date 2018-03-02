287da60bca86fbcef4ba17625f4080d0c86a0f6f
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-03-02 14:26:55 +0530

    Create numno.c

---
+++ numno.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main()
+{
+	long long num;
+	int count=0;
+	printf("enter any number");
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		count++;
+		num/=10;
+	}
+	printf("total digits %d",count);
+return 0;
+}
+getch();
+}
