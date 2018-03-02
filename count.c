7a32f4dcee176547d4a0bf408b8e3237c4ea32b4
NaveenNaresh <35652419+NaveenNaresh@users.noreply.github.com>
2018-03-02 14:06:14 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+int i,n; 
+char a[70];
+printf("enter the value of n and a");
+scanf("%s%d",&a,&n);
+printf("\n %s%d",a,n);
+for(i=1;i<=n;i++)
+{
+	printf("%s",a);
+}
+return 0;
+}
+grtch();
+}
